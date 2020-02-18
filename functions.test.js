b7a9c2d0240515246f671b8b9e8855f3dd31d5f7
Muhammad Naufil <m.naufil1@gmail.com>
2020-02-18 16:30:26 +0500

    Added not.toBe test

--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,6 @@ const functions = require("./functions");
 test("Adds 2 + 2 equals 4",()=>{
     expect(functions.add(2,2)).toBe(4);
 })
+test("Adds 2 + 2 not equals 5",()=>{
+    expect(functions.add(2,2)).not.toBe(5);
+})
