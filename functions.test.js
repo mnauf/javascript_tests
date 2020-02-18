522398143097bcec70c8a96036513fd06461bfd7
Muhammad Naufil <m.naufil1@gmail.com>
2020-02-18 16:49:15 +0500

    added more tests

--- functions.test.js
+++ functions.test.js
@@ -6,6 +6,12 @@ test("Adds 2 + 2 equals 4",()=>{
 test("Adds 2 + 2 not equals 5",()=>{
     expect(functions.add(2,2)).not.toBe(5);
 })
-test("Adds 2 + 2 not equals 5",()=>{
+test("Adds 2 + 2 to be a truth value ",()=>{
     expect(functions.add(2,2)).toBeTruthy();
+})
+test("Subtracting 2 - 2 equals 0",()=>{
+    expect(functions.sub(2,2)).toBe(0);
+})
+test("Subtracting 2 - 2 not equals -5",()=>{
+    expect(functions.sub(2,2)).not.toBe(-5);
 })
