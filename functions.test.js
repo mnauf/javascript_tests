6a02ba2a8836998dbbed71568ce40d30d3cc0d8a
Muhammad Naufil <m.naufil1@gmail.com>
2020-02-18 16:36:44 +0500

    added tobeTruthy() test

--- functions.test.js
+++ functions.test.js
@@ -6,3 +6,6 @@ test("Adds 2 + 2 equals 4",()=>{
 test("Adds 2 + 2 not equals 5",()=>{
     expect(functions.add(2,2)).not.toBe(5);
 })
+test("Adds 2 + 2 not equals 5",()=>{
+    expect(functions.add(2,2)).toBeTruthy();
+})
